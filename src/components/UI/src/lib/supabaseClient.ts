5b64d676371bf7da510191ed013c20380f0828ab
mohamedaboelghare9009as-crypto <mohamedaboelghare9009as@gmail.com>
2025-09-29 07:42:08 +0300

    Update supabaseClient.ts

--- src/components/UI/src/lib/supabaseClient.ts
+++ src/components/UI/src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.https://faicnecedxaifdqsgkhu.supabase.co!;
+const supabaseAnonKey = process.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhaWNuZWNlZHhhaWZkcXNna2h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNTQxNzQsImV4cCI6MjA3NDYzMDE3NH0.d9maQiR47XPRscsuECss6TSfVR-MU6rv1wZZ5o83npk;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
